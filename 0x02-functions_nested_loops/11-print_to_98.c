16b47ebf909ad7df12feb4fbcd8613687ccaa2e2
Theodore Armah <theodorestep@gmail.com>
2023-02-22 13:07:26 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98
+ * @n: paramter to compute
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+	int m;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (m = n; m <= 98; m++)
+		{
+			printf("%d", m);
+			if (m != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (m = n; m <= 98; m++)
+		{
+			printf("%d", m);
+			if (m != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (m = n; m >= 98; m--)
+		{
+			printf("%d", m);
+			if (m != 98)
+				printf(", ");
+		}
+			printf("\n");
+	}
+	else if (n == 98)
+		printf("%d", n);
+
+}
